6b352daa55bb9b6e0db94beda719a922a4398186
Shushant Kumar Singh <100391857+intmain10@users.noreply.github.com>
2024-06-02 11:53:24 +0530

    push

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="text-center py-4">
-      <p>&copy; 2024 Coding Tutorials. All Rights Reserved.</p>
+      <p>&copy; 2024 Coding Tutorials. All Rights Reserved.by shushant</p>
     </footer>
   );
 }
